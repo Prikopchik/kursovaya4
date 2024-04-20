5e956f08f5ea016b5a56f139eea960dee6f36392
agolovanov <alexandergolovanovvla@gmail.com>
2024-04-20 11:16:19 +0300

    add changes

--- src/head_hunter_api.py
+++ src/head_hunter_api.py
@@ -1,21 +1,21 @@
-from .vacancy import Vacancy
+from src.vacancy import Vacancy
+from src.vacancy_api import VacancyAPI
+import requests
+from urllib.parse import urljoin
 
-class HeadHunterAPI:
-    def __init__(self, api_client, json_saver):
-        self.api_client = api_client
-        self.json_saver = json_saver
 
-    def get_vacancies(self, search_query):
-        vacancies_data = self.api_client.get_vacancies(search_query)
-        self.json_saver.save_vacancies_to_json(vacancies_data, "data/vacancies.json")
-        return vacancies_data
+class HHApiClient(VacancyAPI):
+    base_url = 'https://api.hh.ru'
 
-    def filter_vacancies(self, search_query, file_path):
-        vacancies = self.json_saver.load_vacancies_from_json(file_path)
-        filtered_vacancies = [vacancy for vacancy in vacancies if search_query in vacancy['name']]
-        return filtered_vacancies
-
-    def delete_vacancy(self, vacancy_id, file_path):
-        vacancies = self.json_saver.load_vacancies_from_json(file_path)
-        vacancies = [vacancy for vacancy in vacancies if vacancy['id'] != vacancy_id]
-        self.json_saver.save_vacancies_to_json(vacancies, file_path)
+    def get_vacancies(self, search_query: str) -> Vacancy:
+        url = urljoin(self.base_url, 'vacancies')
+        params = {'text': search_query}
+        try:
+            response = requests.get(url, params=params)
+            response.raise_for_status()
+            vacancies_data = response.json()['items']
+            vacancies = [Vacancy(data) for data in vacancies_data]
+            return vacancies
+        except requests.RequestException as e:
+            print("Ошибка при получении данных:", e)
+            return []
--- src/vacancy_api.py
+++ src/vacancy_api.py
@@ -1,22 +1,7 @@
 from abc import ABC, abstractmethod
+from vacancy import Vacancy
 
 class VacancyAPI(ABC):
     @abstractmethod
-    def get_vacancies(self, search_query):
-        pass
-
-    @abstractmethod
-    def add_vacancy(self, vacancy_data, file_path):
-        pass
-
-    @abstractmethod
-    def filter_vacancies(self, search_query, file_path):
-        pass
-
-    @abstractmethod
-    def delete_vacancy(self, vacancy_id, file_path):
-        pass
-
-    @abstractmethod
-    def save_vacancies_to_json(self, vacancies_data, file_path):
+    def get_vacancies(self, search_query: str) -> Vacancy:
         pass
